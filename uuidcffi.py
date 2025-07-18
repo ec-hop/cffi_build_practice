aeb7d259fce2e64a9b7e2b9d7e010e9e9dc987a0
Emma <emma22hopkins@gmail.com>
2025-07-18 14:14:30 -0700

    Update uuidcffi.py

--- uuidcffi.py
+++ uuidcffi.py
@@ -4,9 +4,15 @@
 import uuid
 import _uuidcffi
 
-_ffi = _uuicdffi.ffi
+_ffi = _uuidcffi.ffi
 _lib = _uuidcffi.lib
 
 def generate_uuid() -> uuid.UUID:
   #use the ffi library to construct various types
-  
+  buf = _ffi.new("uuid_t") # ffi.new will manage creating type and deleting when out of scope
+  _lib.uuid_generate(buf)
+  return uuid.UUID(bytes= bytes(buf))
+
+
+                  
+                   
